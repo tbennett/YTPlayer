6f2f64b242bbbfb8918874062b5c086132d03e2f
Troy <tbennett@dvc.edu>
2022-03-10 01:15:52 -0800

    added cuepoint functionality

--- j/main.js
+++ j/main.js
@@ -6,10 +6,7 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
-var done = false;
-var myTimer = '';
-
-
+var myTimer = false;
 
 
 // 3. This function creates an <iframe> (and YouTube player)
@@ -18,7 +15,7 @@ function onYouTubeIframeAPIReady() {
     player = new YT.Player("player", {
         height: "390",
         width: "640",
-        videoId: "SLdZEgDUojc",
+        videoId: "YIjWwZwlHQg",
         playerVars: {
             playsinline: 1,
         },
@@ -27,51 +24,94 @@ function onYouTubeIframeAPIReady() {
             onStateChange: onPlayerStateChange,
         },
     });
-
-    player.addEventListener("onStateChange", (e) => {
-        console.log(e.target);
-    });
-
 }
 
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    console.log('playerReady');
+}
 
 // 5. The API calls this function when the player's state changes.
 //    The function indicates that when playing a video (state=1),
 //    the player should play for 15 seconds and then stop.
 
 function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        //setTimeout(stopVideo, 15000);
-        done = true;
+    
+    if (event.data == YT.PlayerState.PLAYING) {
+        console.log('starting timer');
+        myTimer = setInterval(getTime, 1000, event);
 
-        myTimer = setInterval(getTime, 1000);
-
-        function getTime() {
-            let time = Math.floor(event.target.getCurrentTime());
-            console.log(time);
-
-            if(time >= 6) {
-                stopVideo();
-                doStuff();
-            }
+    } else if (event.data !== YT.PlayerState.PLAYING) {
+        if (!myTimer) {
+            console.log('no timer?');
         }
- 
-    } else {
-        clearInterval(myTimer);
+        else {
+            clearInterval(myTimer);
+            console.log('stopping timer');
+
+        }
     }
 }
 
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-    event.target.playVideo();
+function getTime(event) {
+    time = Math.floor(event.target.getCurrentTime());
+    manageCues(time);
 }
 
 function stopVideo() {
+    console.log('stopVideo');
     player.stopVideo();
 }
 
-// https://developers.google.com/youtube/iframe_api_reference
+function manageCues(time) {
+    console.log(time);
+
+    if(time == 6) {
+        doStuff();
+        player.pauseVideo();
+        setTimeout('player.playVideo();', 5000);
+    }
+
+    if(time == 10) {
+        doMoreStuff();
+    }
+
+    if(time == 20) {
+        changeLayout();
+        showInfo();
+    }
+}
+
+
+/**
+ * Below are all of the functions called by the 
+ * manageCues function
+ * 
+ */
 
 function doStuff() {
+    console.log('doStuff');
     document.body.style.backgroundColor = "red";
+}
+
+
+function doMoreStuff() {
+    document.body.style.backgroundColor = "green";
+    console.log('moreStuffDone');
+}
+
+function changeLayout() {
+    let iframe = player.getIframe();
+    iframe.classList.add('layout2');
+    player.getIframe().style.border = '10px solid red';
+    console.log('layout changed');
+}
+
+function showInfo() {
+    const info = document.getElementById('info');
+    const iframe = document.createElement('IFRAME');
+    iframe.classList.add('myframe');
+    iframe.src = "https://en.wikipedia.org/wiki/Steak";
+    info.appendChild(iframe);
 }
