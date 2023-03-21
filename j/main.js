18a5eda8696187f1a38ff31180b50960b6bec322
Troy <tbennett@dvc.edu>
2023-03-21 00:27:34 -0700

    turn off cuepoints when video is switched.

--- j/main.js
+++ j/main.js
@@ -1,6 +1,6 @@
-/** @format */
 
-// 2. This code loads the IFrame Player API code asynchronously.
+
+// 1. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement("script");
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName("script")[0];
@@ -9,7 +9,7 @@ var player;
 var myTimer = false;
 
 
-// 3. This function creates an <iframe> (and YouTube player)
+// 2. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
 function onYouTubeIframeAPIReady() {
     player = new YT.Player("player", {
@@ -26,10 +26,12 @@ function onYouTubeIframeAPIReady() {
     });
 }
 
-//video control buttons
+// 3. setup the video control buttons
 const myplay = document.getElementById('myplay');
 const mypause = document.getElementById('mypause');
 const slo = document.getElementById('slo');
+const normal = document.getElementById('normal');
+const fast = document.getElementById('fast');
 
 //add event listners for controls
 myplay.addEventListener('click', (e) => {
@@ -48,6 +50,10 @@ normal.addEventListener('click', (e) => {
     player.setPlaybackRate(1);
 });
 
+fast.addEventListener('click', (e) => {
+    player.setPlaybackRate(2);
+});
+
 // 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
     console.log('playerReady');
@@ -55,66 +61,69 @@ function onPlayerReady(event) {
 
 // 5. The API calls this function when the player's state changes.
 //    The function indicates that when playing a video (state=1),
-//    the player should play for 15 seconds and then stop.
 
 function onPlayerStateChange(event) {
-    
-    if (event.data == YT.PlayerState.PLAYING) {
-        console.log('starting timer');
-        myTimer = setInterval(getTime, 1000, event);
-
-    } else if (event.data !== YT.PlayerState.PLAYING) {
-        if (!myTimer) {
-            console.log('no timer?');
-        }
-        else {
+    switch(event.data) {
+        case YT.PlayerState.PLAYING:
+            console.log('starting timer');
+            myTimer = setInterval(getTime, 1000, event);
+            break;
+        case !YT.PlayerState.PLAYING:
+            if (!myTimer) {
+                console.log('no timer');
+            }
+            break;
+        default:
             clearInterval(myTimer);
             console.log('stopping timer');
-
-        }
     }
 }
 
+// 6. get the currentTime of the video and trigger the
+// manageCues function if we are watching the inital video.
 function getTime(event) {
+    let vidInfo = event.target.getVideoData();
     time = Math.floor(event.target.getCurrentTime());
-    manageCues(time);
+
+    // make it so the manageCues only runs for a specific video
+    if (vidInfo.video_id == 'YIjWwZwlHQg') {
+        manageCues(time);
+    } else {
+        console.log('cue NOT managed');
+    }
 }
 
-function stopVideo() {
-    console.log('stopVideo');
-    player.stopVideo();
-}
 
+ // This function acts as the playlist for the inital video
 function manageCues(time) {
     console.log(time);
 
-    if(time == 3) {
-        doStuff();
-    }
-
-    if(time == 10) {
-        doMoreStuff();
-    }
-
-    if(time == 21) {
-        changeLayout();
-        showInfo();
-        player.pauseVideo();
-        setTimeout('player.playVideo();', 5000);
-    }
-
-    // swapping out a video on the fly!
-    // be mindful that once the new video is swapped out,
-    // it will be affected by the timings of the manageCues function.
-    // to avoid, I'm seeking past the last cue of 26 seconds.
-    if(time == 26) {
-        player.loadVideoById({
-            'videoId': '_QtUGdaCb1c',
-            'startSeconds': 30,
-            'endSeconds': 60
-        });
+    switch(time) {
+        case 3:
+            doStuff();
+            break;
+        case 10:
+            doMoreStuff();
+            break;
+        case 21:
+            changeLayout();
+            showInfo();
+            player.pauseVideo();
+            setTimeout('player.playVideo();', 5000);
+            break;
+        case 26:
+            player.loadVideoById({
+                'videoId': '_QtUGdaCb1c',
+                'startSeconds': 0,
+                'endSeconds': 1000
+            });
+            clearInterval(myTimer);
+    
+            //undo previous styling caused by inital video
+            unDoStuff();
     }
 }
+
 
 
 /**
@@ -122,6 +131,7 @@ function manageCues(time) {
  * manageCues function
  * 
  */
+
 
 function doStuff() {
     console.log('doStuff');
@@ -147,4 +157,27 @@ function showInfo() {
     iframe.classList.add('myframe');
     iframe.src = "https://www.montalvospirits.com/how-long-should-you-let-steaks-warm-up-before-cooking/";
     info.appendChild(iframe);
+}
+
+
+//If the video is switched from initial video, calling this function will undo DOM changed made for the first video.
+function unDoStuff() {
+    document.body.style.backgroundColor = "white";
+
+    //clear iFrame
+    const infoFrame = document.querySelector('.myframe');
+    infoFrame.src ='';
+    // clear the article
+    document.querySelector('#info').innerHTML = '';
+    
+    //re-style the page
+    let iframe = player.getIframe();
+    iframe.classList.remove('layout2');
+    player.getIframe().style.border = 'none';
+    console.log('UNDONE!!!!!!!');
+}
+
+function stopVideo() {
+    console.log('video stopped');
+    player.stopVideo();
 }
